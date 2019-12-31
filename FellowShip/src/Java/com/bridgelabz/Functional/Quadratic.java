900c973d079d301e1de8ee3b1ec123ad06815242
Lavanya560 <lavanya7.manduri@gmail.com>
2019-12-31 11:51:41 +0530

    [lavanya].Added Basic program, logical program,functional progarm,Data structures,algorithms,junit

--- FellowShip/src/Java/com/bridgelabz/Functional/Quadratic.java
+++ FellowShip/src/Java/com/bridgelabz/Functional/Quadratic.java
@@ -3,9 +3,9 @@ package com.bridgelabz.Functional;
  *  Purpose: Program is written for Quadratic .
  *  		 
  *
- *  @author  Manduri Lavanya<>
+ *  @author  Manduri Lavanya<lavanya.manduri@gmail.com>
  *  @version 1.0
- *  @since   14-12-2020
+ *  @since   14-12-2019
  *
  ******************************************************************************/
 import com.bridgelabz.Utility.Utility;
