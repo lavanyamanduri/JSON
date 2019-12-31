5325cf0f0a48a24304400df52116f663d933c865
Lavanya560 <lavanya7.manduri@gmail.com>
2019-12-31 11:25:47 +0530

    [lavanya].Added Basic program, logical program,functional progarm,Data structures,algorithms,junit

--- FellowShip/src/Java/com/bridgelabz/DataStructure/Dequeue.java
+++ FellowShip/src/Java/com/bridgelabz/DataStructure/Dequeue.java
@@ -3,9 +3,9 @@ package com.bridgelabz.DataStructure;
  *  Purpose: Program is written for Dequeue  ( Reusability of Code ) .
  *  		 
  *
- *  @author  Manduri Lavanya<>
+ *  @author  Manduri Lavanya<lavanya.manduri@gmail.com>
  *  @version 1.0
- *  @since   21-12-2020
+ *  @since   21-12-2019
  *
  ******************************************************************************/
 public class Dequeue<T> {
