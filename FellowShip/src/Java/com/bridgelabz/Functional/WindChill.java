31790e2a46eb3e6cba3c057777faeddce1d8e4f3
Lavanya560 <lavanya7.manduri@gmail.com>
2019-12-31 11:53:00 +0530

    [lavanya].Added Basic program, logical program,functional progarm,Data structures,algorithms,junit

--- FellowShip/src/Java/com/bridgelabz/Functional/WindChill.java
+++ FellowShip/src/Java/com/bridgelabz/Functional/WindChill.java
@@ -3,9 +3,9 @@ package com.bridgelabz.Functional;
  *  Purpose: Program is written for WindChill .
  *  		 
  *
- *  @author  Manduri Lavanya<>
+ *  @author  Manduri Lavanya<lavanya.manduri@gmail.com>
  *  @version 1.0
- *  @since   13-12-2020
+ *  @since   13-12-2019
  *
  ******************************************************************************/
 import com.bridgelabz.Utility.Utility;
