320a3b32ba21e786b60a7d888478fa4e38721821
Lavanya560 <lavanya7.manduri@gmail.com>
2019-12-31 11:05:18 +0530

    [lavanya] Added Basic program, logical program,functional progarm,Data structure, alogorithm, junit

--- FellowShip/src/Java/com/bridgelabz/Basic/PowerOf2.java
+++ FellowShip/src/Java/com/bridgelabz/Basic/PowerOf2.java
@@ -3,9 +3,9 @@ package com.bridgelabz.Basic;
  *  Purpose: Program is written for PowerOf2 .
  *  		 
  *
- *  @author  Manduri Lavanya<>
+ *  @author  Manduri Lavanya<lavanya.manduri@gmail.com>
  *  @version 1.0
- *  @since   13-12-2020
+ *  @since   13-12-2019
  *
  ******************************************************************************/
 import com.bridgelabz.Utility.Utility;
