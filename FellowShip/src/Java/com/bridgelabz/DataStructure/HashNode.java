f9d6818cbf1ffeed47d03e9d30f69937acd09f31
Lavanya560 <lavanya7.manduri@gmail.com>
2019-12-31 11:27:26 +0530

    [lavanya].Added Basic program, logical program,functional progarm,Data structures,algorithms,junit

--- FellowShip/src/Java/com/bridgelabz/DataStructure/HashNode.java
+++ FellowShip/src/Java/com/bridgelabz/DataStructure/HashNode.java
@@ -3,9 +3,9 @@ package com.bridgelabz.DataStructure;
  *  Purpose: Program is written for HashNode  ( Reusability of Code ) .
  *  		 
  *
- *  @author  Manduri Lavanya<>
+ *  @author  Manduri Lavanya<lavanya.manduri@gmail.com>
  *  @version 1.0
- *  @since   23-12-2020
+ *  @since   23-12-2019
  *
  ******************************************************************************/
 	public class HashNode<K, V> {
