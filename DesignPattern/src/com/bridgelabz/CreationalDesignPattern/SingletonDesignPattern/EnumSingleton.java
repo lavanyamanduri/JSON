938be6176817773a85d48a39b0699103663517b4
Lavanya560 <lavanya7.manduri@gmail.com>
2020-01-15 22:38:57 +0530

    [lavanya] DesignPatterns Creational done

---
+++ DesignPattern/src/com/bridgelabz/CreationalDesignPattern/SingletonDesignPattern/EnumSingleton.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.CreationalDesignPattern.SingletonDesignPattern;
+
+public enum EnumSingleton {
+
+    INSTANCE;
+    
+    public static void doSomething(){
+        //do something
+    }
+}
